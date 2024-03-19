2ed0eee54e5aa51cf913c430bd4c9ebaa5cfdb36
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2024-03-19 18:11:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const ports = 4000 || process.env.PORTS
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
-app.get('/test',(req,res)=>{
-    res.send('hello world')
-})
-
 app.listen(ports,()=>{
      console.log(`App listening on port ${ports}`);
 })
