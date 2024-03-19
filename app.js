78b81d65d1d0405ad8fcf82a004c39227b403d83
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2024-03-19 18:27:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,8 @@ const express = require('express')
 const app = express();
 const ports = 4000 || process.env.PORTS
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
 app.get('/',(req,res)=>{
-    res.json({
-        message:"Hello World!"
-    });
-})
-app.get('/test',(req,res)=>{
-    res.json({
-        message:"Hello World!"
-    });
+    res.send('hello world')
 })
 app.listen(ports,()=>{
      console.log(`App listening on port ${ports}`);
