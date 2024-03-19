4c745141d2386bb1536e3d9bb26706fca5df6296
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2024-03-19 18:04:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express();
 const ports = 4000 || process.env.PORTS
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const route = require('./router');
+app.use(route);
+
 app.get('/',(req,res)=>{
-    res.send('hello world')
+    res.json({
+        message:"Hello World!"
+    });
 })
 
 app.listen(ports,()=>{
