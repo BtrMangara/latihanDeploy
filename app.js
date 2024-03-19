2359f23723c129ebb9cd494e2a4d4bae19b35153
Mangara Butarbutar <120437609+BtrMangara@users.noreply.github.com>
2024-03-19 18:00:36 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const ports = 4000 || process.env.PORTS
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+app.listen(ports,()=>{
+    console.log(`App listening on port ${ports}`);
+})
